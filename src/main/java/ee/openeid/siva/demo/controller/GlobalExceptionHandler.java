f9ecc8678758411116d92d5535d5c8295f0b0195
nibbydev <nibby.rindell@gmail.com>
2023-02-13 10:36:18 +0200

    SIVA-393 Fix MaxUploadSizeExceededException not handled

--- src/main/java/ee/openeid/siva/demo/controller/GlobalExceptionHandler.java
+++ src/main/java/ee/openeid/siva/demo/controller/GlobalExceptionHandler.java
@@ -1,13 +1,14 @@
 package ee.openeid.siva.demo.controller;
 
-import lombok.Data;
-
 import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import lombok.Data;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,6 +19,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(SizeLimitExceededException.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse sizeLimitException(SizeLimitExceededException exception) {
+        return handleSizeLimitException();
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse sizeLimitException(MaxUploadSizeExceededException exception) {
+        return handleSizeLimitException();
+    }
+
+    private ErrorResponse handleSizeLimitException() {
         ErrorResponse errorResponse = new ErrorResponse();
         errorResponse.setErrorMessage(String.format("Maximum upload size of %s exceeded", maxRequestSize));
         errorResponse.setErrorCode("INTERNAL_SERVER_ERROR");
